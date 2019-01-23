9dec769ea205f79b9dafaf211d73e66e1016146a
Josmer Hernandez <jhernandez.18p@gmail.com>
2019-01-22 23:32:44 -0500

    updates

--- app/settings.py
+++ app/settings.py
@@ -43,8 +43,8 @@ else:
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_SSL = True
-EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_PORT = 565
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
